db6b3211cfdf46e03d4de6c3908d0444b3889d86
Rajnish0202 <rajnish.0202kumar@gmail.com>
2023-04-23 21:25:15 +0530

    server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://shopnow-app.vercel.app'],
     credentials: true,
   })
 );
